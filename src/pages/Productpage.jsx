5bcff01026337b88f9a90132762459c7fd86510c
vikash11995 <vikashyadav11995@gmail.comm>
2025-09-25 17:27:12 +0530

    resolve product not found

--- src/pages/Productpage.jsx
+++ src/pages/Productpage.jsx
@@ -5,6 +5,12 @@ const products = [
   { id: "p1", name: "Laptop", price: 899, desc: "Powerful laptop for work and play.", imgUrl: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTsNo2hygc2YHMHwju5cfLszjvRGehIPHi2cfi1QgsUSRBUpQs8IVrOPbdelEeJ4M_K00I4c6i7Tm7OhQyRUeieQkXT8FkzOdl_PCgXLwB1" },
   { id: "p2", name: "Gaming Mouse", price: 49, desc: "High precision RGB gaming mouse.", imgUrl: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSJSU7fUqKErMjArnczkjg2I0gkeBqv423pIMNSeolnrDk-oxHLf2rd02948ikOF12zCWr6AETFHaYVgau53hpPNm9HkkOs1QN3WntWOnkzc5fhA1e0rDTo" },
   { id: "p3", name: "Keyboard", price: 79, desc: "Mechanical keyboard with backlight.", imgUrl: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTi78qtSOjaXJh2WJtdcWjyNmNHvEFAueuwcH0V8DAOVG07rErXOeWTiHWrNE2G571pUVZhWrhs143Qh6vBwDeMGwGpF7ACWALr6y8aXle8b4A4VG0ypxlayP0" },
+  { id: "p4", name: "Laptop", price: 899, desc: "Powerful laptop for work and play.", imgUrl: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTsNo2hygc2YHMHwju5cfLszjvRGehIPHi2cfi1QgsUSRBUpQs8IVrOPbdelEeJ4M_K00I4c6i7Tm7OhQyRUeieQkXT8FkzOdl_PCgXLwB1" },
+  { id: "p5", name: "Gaming Mouse", price: 49, desc: "High precision RGB gaming mouse.", imgUrl: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSJSU7fUqKErMjArnczkjg2I0gkeBqv423pIMNSeolnrDk-oxHLf2rd02948ikOF12zCWr6AETFHaYVgau53hpPNm9HkkOs1QN3WntWOnkzc5fhA1e0rDTo" },
+  { id: "p6", name: "Keyboard", price: 79, desc: "Mechanical keyboard with backlight.", imgUrl: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTi78qtSOjaXJh2WJtdcWjyNmNHvEFAueuwcH0V8DAOVG07rErXOeWTiHWrNE2G571pUVZhWrhs143Qh6vBwDeMGwGpF7ACWALr6y8aXle8b4A4VG0ypxlayP0" },
+  { id: "p7", name: "Laptop", price: 899, desc: "Powerful laptop for work and play.", imgUrl: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTsNo2hygc2YHMHwju5cfLszjvRGehIPHi2cfi1QgsUSRBUpQs8IVrOPbdelEeJ4M_K00I4c6i7Tm7OhQyRUeieQkXT8FkzOdl_PCgXLwB1" },
+  { id: "p8", name: "Gaming Mouse", price: 49, desc: "High precision RGB gaming mouse.", imgUrl: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSJSU7fUqKErMjArnczkjg2I0gkeBqv423pIMNSeolnrDk-oxHLf2rd02948ikOF12zCWr6AETFHaYVgau53hpPNm9HkkOs1QN3WntWOnkzc5fhA1e0rDTo" },
+  { id: "p9", name: "Keyboard", price: 79, desc: "Mechanical keyboard with backlight.", imgUrl: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTi78qtSOjaXJh2WJtdcWjyNmNHvEFAueuwcH0V8DAOVG07rErXOeWTiHWrNE2G571pUVZhWrhs143Qh6vBwDeMGwGpF7ACWALr6y8aXle8b4A4VG0ypxlayP0" },
 ];
 
 export default function ProductPage() {
